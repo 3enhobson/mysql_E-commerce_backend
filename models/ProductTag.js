de5e3adbb38de8b329b43fb721756f6a5c85be6b
Ben Hobson <benhobson@Bens-MBP.attlocal.net>
2022-10-03 15:03:24 -0700

    orginal commit, Installed dependencies, created env file, started to define columns

---
+++ models/ProductTag.js
@@ -0,0 +1,27 @@
+const { Model, DataTypes } = require('sequelize');
+
+const sequelize = require('../config/connection');
+
+class ProductTag extends Model {}
+
+ProductTag.init(
+  {
+    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+   //Not sure which ID to use for the column
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'product_tag',
+  }
+);
+
+module.exports = ProductTag;
